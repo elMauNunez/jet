12408068489a9312725fad0da0e0405598af856b
Javier Jiménez <themjnunez@gmail.com>
2015-08-19 09:53:25 -0500

    Handle Request
    Given a file's name and size, return a function that handles a response
    validating its status and getting the total size of the file to download.

--- bin.js
+++ bin.js
@@ -14,15 +14,33 @@ var fileSize = function (name, callback) {
   });
 };
 
-var request = function (url) {
+var badStatus = function (statusCode) {
+  return Number(statusCode) >= 400;
+};
+
+var handleData = function (size, name) {
+  return function (data) {
+    console.log(data);
+  };
+};
+
+var handleResponse = function (size, name) {
+  return function (res) {
+    if (badStatus(res.statusCode)) throw res.statusMessage;
+    var totalFileSize = size + Number(res.headers['content-length']);
+    res.on('data', handleData(totalFileSize, name));
+  };
+};
+
+var request = function (url, name) {
   return function (size) {
     var req = hyperquest(url);
     req.setHeader('Range', 'bytes=' + size + '-');
-    req.on('response', console.log);
+    req.on('response', handleResponse(size, name));
   };
 };
 
 var get = function (url) {
   var name = fileName(url);
-  fileSize(name, request(url));
+  fileSize(name, request(url, name));
 };
