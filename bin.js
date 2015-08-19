32f8255af388b29049bf38cea2fc1527310a7f36
Javier Jiménez <themjnunez@gmail.com>
2015-08-19 09:23:25 -0500

    Get File Size
    Given a file name, get the its size and pass it to a callback function.
    
    If there is no file, size is 0.

--- bin.js
+++ bin.js
@@ -1,3 +1,14 @@
+var fs = require('fs');
+
 var fileName = function (url) {
   return url.split('/').pop() || 'index.html';
+};
+
+var fileSize = function (name, callback) {
+  fs.stat(name, function (err, stats) {
+    if (!stats)
+      return callback(0);
+
+    return callback(stats.size);
+  });
 };
